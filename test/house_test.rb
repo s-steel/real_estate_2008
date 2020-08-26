c5639f3a87378412bcbdd621c5f066f2f7ecbccd
Sean Steel <seanrsteel@gmail.com>
2020-08-26 09:30:17 -0600

    Add add_room method and tests

--- test/house_test.rb
+++ test/house_test.rb
@@ -18,5 +18,15 @@ class HouseTest < Minitest::Test
     assert_equal [], house.rooms
   end
 
+  def test_add_room
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
 
+    house.add_room(room_1)
+    assert_equal [room_1], house.rooms
+
+    house.add_room(room_2)
+    assert_equal [room_1, room_2], house.rooms
+  end
 end
