77a73c8f87901630bd48a472223649dd469edda1
Sean Steel <seanrsteel@gmail.com>
2020-08-26 09:14:41 -0600

    Add room class and confirm tests

---
+++ lib/room.rb
@@ -0,0 +1,25 @@
+class Room
+  attr_reader :category, :length, :width, :painted
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+    @painted = false
+  end
+
+  def area
+    length * (width.to_i)
+  end
+
+  def paint
+    @painted = true
+  end
+
+  def is_painted?
+    if @painted == true
+      true
+    else
+      false
+    end
+  end
+end
